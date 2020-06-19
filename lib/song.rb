2359429257e2a51bda7a65ea96cb8f88c3da2a2d
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-19 15:12:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,6 @@ class Song
     @@all << self
   end
   
-  def name=(name)
-    @name = name
-  end
-  
   def self.new_by_filename(file)
     song = file.split(" - ")[1]
     artist = file.split(" - ")[0]
@@ -21,10 +17,6 @@ class Song
   def artist_name=(a_name)
     self.artist = Artist.find_or_create_by_name(a_name)
     artist.add_song(self)
-  end
-  
-  def self.all 
-    @@all 
   end
   
 end
