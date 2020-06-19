4a159e221e62e17eaf45709b633ff77a8d731276
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-19 15:04:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,13 +11,13 @@ class Song
     @name = name
   end
   
-  def self.new_by_filename(file)
-    song = file.split(" - ")[1]
-    artist = file.split(" - ")[0]
-    new_tune = self.new(song)
-    new_tune.artist_name = artist
-    new_tune
-  end 
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
   
   def artist_name=(a_name)
     if self.artist.nil?
